6a6d5c03954fb1671b42305f5f748410b7e905fc
TranPhuDev <tranphudev3110@gmail.com>
2025-05-14 00:49:21 +0700

    test form

---
+++ 01-java-spring-laptopshop-starter-master/src/main/java/vn/tranphudev/laptopshop/controller/UserController.java
@@ -0,0 +1,41 @@
+package vn.tranphudev.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import vn.tranphudev.laptopshop.domain.User;
+import vn.tranphudev.laptopshop.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("phu", test);
+        model.addAttribute("phutran", "From controller with model");
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUser(Model model) {
+        model.addAttribute("newUser", new User());
+
+        return "/admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUser(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println("Run here" + hoidanit);
+        return "hello";
+    }
+}
--- 01-java-spring-laptopshop-starter-master/src/main/java/vn/tranphudev/laptopshop/services/MyServices.java
+++
@@ -1,8 +0,0 @@
-package vn.tranphudev.laptopshop.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class MyServices {
-
-}
